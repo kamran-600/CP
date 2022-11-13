b1309543d3f09da5af24a3bcba486dc55c0fd914
P-I-K-U <ksurajyadav2021@gmail.com>
2022-11-13 20:20:44 +0530

    Bottom nav bar

--- app/src/main/java/com/example/collegeproject/MainActivity.java
+++ app/src/main/java/com/example/collegeproject/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+               Intent intent = new Intent(MainActivity.this,HomeActivity.class);
                startActivity(intent);
             }
         });
