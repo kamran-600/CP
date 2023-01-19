ad544ef9e679ed85f0e864e5004432c5b3cc5e96
P-I-K-U <ksurajyadav2021@gmail.com>
2023-01-19 16:48:26 +0530

    all done by s

---
+++ app/src/main/java/com/example/collegeproject/Chat/ConversationActivity.java
@@ -0,0 +1,14 @@
+package com.example.collegeproject.Chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConversationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation);
+    }
+}
